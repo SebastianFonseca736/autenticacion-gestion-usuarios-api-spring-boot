6cd082a376f5db30dfb8bfc4961d4fe321e045b8
Sebastian <s.fonseca@duocuc.cl>
2025-05-23 18:34:18 -0400

    Primera subida

---
+++ src/main/java/com/example/autenticacion_gestion_usuarios_api_spring_boot/AutenticacionGestionUsuariosApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.autenticacion_gestion_usuarios_api_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticacionGestionUsuariosApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticacionGestionUsuariosApiSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/autenticacion_gestion_usuarios_api_spring_boot/AutenticacionGestionUsuariosApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autenticacion_gestion_usuarios_api_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticacionGestionUsuariosApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
